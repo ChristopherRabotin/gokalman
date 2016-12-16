29576b213ad13cf6b023152915e3bc46e54c72c4
Christopher Rabotin <christopher.rabotin@gmail.com>
2016-12-16 03:35:36 -0700

    Implemented Monte Carlo runs.

---
+++ montecarlo.go
@@ -0,0 +1,110 @@
+package gokalman
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gonum/matrix/mat64"
+	"github.com/gonum/stat"
+)
+
+// MonteCarloRuns stores MC runs.
+type MonteCarloRuns struct {
+	runs, steps int
+	Runs        []MonteCarloRun
+}
+
+// Mean returns the mean of all the samples for the given time step.
+func (mc MonteCarloRuns) Mean(step int) (mean []float64) {
+	// Take the first run in order to know the size.
+	states := make(map[int][]float64)
+	rows, _ := mc.Runs[0].Estimates[0].State().Dims()
+	for i := 0; i < rows; i++ {
+		states[i] = make([]float64, len(mc.Runs))
+	}
+	// Gather information
+	for r, run := range mc.Runs {
+		state := run.Estimates[step].State()
+		for i := 0; i < rows; i++ {
+			states[i][r] = state.At(i, 0)
+		}
+	}
+	means := make([]float64, rows)
+	for i := 0; i < rows; i++ {
+		means[i] = stat.Mean(states[i], nil)
+	}
+	return means
+}
+
+// StdDev returns the standard deviation of all the samples for the given time step.
+func (mc MonteCarloRuns) StdDev(step int) (mean []float64) {
+	// Take the first run in order to know the size.
+	states := make(map[int][]float64)
+	rows, _ := mc.Runs[0].Estimates[0].State().Dims()
+	for i := 0; i < rows; i++ {
+		states[i] = make([]float64, len(mc.Runs))
+	}
+	// Gather information
+	for r, run := range mc.Runs {
+		state := run.Estimates[step].State()
+		for i := 0; i < rows; i++ {
+			states[i][r] = state.At(i, 0)
+		}
+	}
+	devs := make([]float64, rows)
+	for i := 0; i < rows; i++ {
+		devs[i] = stat.StdDev(states[i], nil)
+		fmt.Printf("%d stddev %d = %f from %+v\n", step, i, devs[i], states[i])
+	}
+	return devs
+}
+
+// AsCSV is used as a CSV serializer. Does not include the header.
+func (mc MonteCarloRuns) AsCSV(headers []string) []string {
+	rows, _ := mc.Runs[0].Estimates[0].State().Dims()
+	rtn := make([]string, rows)
+
+	for i := 0; i < rows; i++ {
+		header := headers[i]
+		lines := make([]string, mc.steps+1) // One line per step, plus header.
+		for rNo := 0; rNo < mc.runs; rNo++ {
+			lines[0] += fmt.Sprintf("%s-%d,", header, rNo)
+		}
+		lines[0] += header + "-mean," + header + "-stddev"
+
+		for k := 0; k < mc.steps; k++ {
+			for rNo, run := range mc.Runs {
+				lines[k+1] += fmt.Sprintf("%f,", run.Estimates[k].State().At(i, 0))
+
+				if rNo == mc.runs-1 {
+					// Last run reached, let's add the mean and stddev for this step.
+					mean := mc.Mean(k)
+					stddev := mc.StdDev(k)
+					lines[k+1] += fmt.Sprintf("%f,%f", mean[i], stddev[i])
+				}
+			}
+		}
+		rtn[i] = strings.Join(lines, "\n")
+	}
+	return rtn
+}
+
+// NewMonteCarloRuns run monte carlos on the provided filter.
+// WARNING: Currently does not support control vectors (yet).
+func NewMonteCarloRuns(samples, steps, colsG, rowsH int, kf KalmanFilter) MonteCarloRuns {
+	runs := make([]MonteCarloRun, samples)
+	for sample := 0; sample < samples; sample++ {
+		MCRun := MonteCarloRun{Estimates: make([]Estimate, steps)}
+		for k := 0; k < steps; k++ {
+			est, _ := kf.Update(mat64.NewVector(rowsH, nil), mat64.NewVector(colsG, nil))
+			MCRun.Estimates[k] = est
+		}
+		runs[sample] = MCRun
+	}
+	return MonteCarloRuns{samples, steps, runs}
+}
+
+// MonteCarloRun stores the results of an MC run.
+type MonteCarloRun struct {
+	Estimates []Estimate
+}
---
+++ montecarlo_test.go
@@ -0,0 +1,39 @@
+package gokalman
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/gonum/matrix/mat64"
+)
+
+func TestMCRuns(t *testing.T) {
+	// Create a Vanilla KF.
+	F, _, Δt := Midterm2Matrices()
+	Q := mat64.NewSymDense(3, []float64{2.5e-15, 6.25e-13, (25e-11) / 3, 6.25e-13, (5e-7) / 3, 2.5e-8, (25e-11) / 3, 2.5e-8, 5e-6})
+	R := mat64.NewSymDense(1, []float64{0.005 / Δt})
+	H := mat64.NewDense(1, 3, []float64{1, 0, 0})
+	G := mat64.NewDense(1, 1, nil)
+	noise := NewAWGN(Q, R)
+	x0 := mat64.NewVector(3, []float64{0, 0.35, 0})
+	P0 := ScaledIdentity(3, 10)
+	kf, _ := NewVanilla(x0, P0, F, G, H, noise)
+
+	runs := NewMonteCarloRuns(5, 10, 1, 1, kf)
+	if len(runs.Runs) != 5 {
+		t.Fatal("requesting 5 runs did not generate five")
+	}
+	for r, run := range runs.Runs {
+		if len(run.Estimates) != 10 {
+			t.Fatalf("sample #%d does not have 10 steps", r)
+		}
+	}
+	files := runs.AsCSV([]string{"x", "y", "z"})
+	if len(files) != 3 {
+		t.Fatal("less than 3 files returned from a three component state")
+	}
+
+	if len(strings.Split(files[0], "\n")) != 11 {
+		t.Fatalf("unexpected number of lines in the file: %d", len(files[0]))
+	}
+}

2c8a04f4803db902cf7d050f9f3e9799b0a03473
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-04-11 00:32:37 -0600

    Cool, sanity check works

--- srif_test.go
+++ srif_test.go
@@ -1,28 +1,22 @@
 package gokalman
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/gonum/matrix/mat64"
 )
 
 func TestSRIFR0(t *testing.T) {
-
-	F, G, Δt := Midterm2Matrices()
-	Q := mat64.NewSymDense(3, []float64{2.5e-15, 6.25e-13, (25e-11) / 3, 6.25e-13, (5e-7) / 3, 2.5e-8, (25e-11) / 3, 2.5e-8, 5e-6})
-	R := mat64.NewSymDense(1, []float64{0.005 / Δt})
-	H := mat64.NewDense(1, 3, []float64{1, 0, 0})
-	noise := NewAWGN(Q, R)
+	noise := Noiseless{}
 	x0 := mat64.NewVector(3, []float64{0, 0.35, 0})
 	P0 := ScaledIdentity(3, 10)
-	_, est0, err := NewSquareRootInformation(x0, P0, F, G, H, noise)
+	_, est0, err := NewSquareRootInformation(x0, P0, noise, 3)
 	if err != nil {
 		t.Fatal(err)
 	}
-	est0.stddev
-	// Sanity check
 
-	fmt.Printf("P0:\n%+v\n\nPc:\n%+v", mat64.Formatted(P0))
+	if !mat64.Equal(est0.Covariance(), P0) {
+		t.Fatalf("difference in P0 and computed covariance:\n%+v\n%+v", mat64.Formatted(P0), mat64.Formatted(est0.Covariance()))
+	}
 
 }

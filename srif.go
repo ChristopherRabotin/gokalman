52f1061ab5d02bdb5fd790db882fe21ee795489e
Christopher Rabotin <christopher.rabotin@gmail.com>
2017-04-11 18:25:07 -0600

    Completes the SRIF algorithm

--- srif.go
+++ srif.go
@@ -8,10 +8,10 @@ import (
 	"github.com/gonum/matrix/mat64"
 )
 
-// NewSquareRootInformation returns a new Square Root Information Filter.
+// NewSRIF returns a new Square Root Information Filter.
 // It uses the algorithms from "Statistical Orbit determination" by Tapley, Schutz & Born.
 // Set upperTriangular to `false` to use the lower triangular of the Cholesky decomposition instead of the upper.
-func NewSquareRootInformation(x0 *mat64.Vector, P0 mat64.Symmetric, measSize int, upperTriangular bool) (*SRIF, *SRIFEstimate, error) {
+func NewSRIF(x0 *mat64.Vector, P0 mat64.Symmetric, measSize int, upperTriangular bool, n Noise) (*SRIF, *SRIFEstimate, error) {
 	// Check the dimensions of each matrix to avoid errors.
 	if err := checkMatDims(x0, P0, "x0", "P0", rows2cols); err != nil {
 		return nil, nil, err
@@ -38,18 +38,28 @@ func NewSquareRootInformation(x0 *mat64.Vector, P0 mat64.Symmetric, measSize int
 	b0 := mat64.NewVector(r, nil)
 	b0.MulVec(&R0, x0)
 
+	// Compute the square root of the measurement noise.
+	var sqrtRchol mat64.Cholesky
+	sqrtRchol.Factorize(n.MeasurementMatrix())
+	var sqrtMeasNoise mat64.TriDense
+	sqrtMeasNoise.LFromCholesky(&sqrtRchol)
+	var sqrtInvNoise mat64.Dense
+	if err := sqrtInvNoise.Inverse(&sqrtMeasNoise); err != nil {
+		return nil, nil, err
+	}
 	// Populate with the initial values.
-	est0 := NewSRIFEstimate(nil, b0, nil, nil, nil, &R0, &R0)
-	return &SRIF{nil, nil, est0, true, measSize, 0}, &est0, nil
+	est0 := NewSRIFEstimate(nil, b0, nil, nil, &R0, &R0)
+	return &SRIF{nil, nil, &sqrtMeasNoise, est0, true, measSize, 0}, &est0, nil
 }
 
 // SRIF defines a square root information filter for non-linear dynamical systems. Use NewSquareRootInformation to initialize.
 type SRIF struct {
-	Φ, Htilde *mat64.Dense
-	prevEst   SRIFEstimate
-	locked    bool // Locks the KF to ensure Prepare is called.
-	measSize  int  // Stores the measurement vector size, needed only for Predict()
-	step      int
+	Φ, Htilde    *mat64.Dense
+	sqrtInvNoise mat64.Matrix
+	prevEst      SRIFEstimate
+	locked       bool // Locks the KF to ensure Prepare is called.
+	measSize     int  // Stores the measurement vector size, needed only for Predict()
+	step         int
 }
 
 // Prepare unlocks the KF ready for the next Update call.
@@ -93,63 +103,26 @@ func (kf *SRIF) fullUpdate(purePrediction bool, realObservation, computedObserva
 	bBar.MulVec(&RBar, &xBar)
 
 	if purePrediction {
-		kf.prevEst = NewSRIFEstimate(kf.Φ, &bBar, mat64.NewVector(kf.measSize, nil), mat64.NewVector(kf.measSize, nil), mat64.NewVector(kf.measSize, nil), &RBar, &RBar)
+		kf.prevEst = NewSRIFEstimate(kf.Φ, &bBar, mat64.NewVector(kf.measSize, nil), mat64.NewVector(kf.measSize, nil), &RBar, &RBar)
 		kf.step++
 		kf.locked = true
 		return
 	}
-	/*
-		// Kalman gain
-		var PHt, HPHt, K mat64.Dense
-		PHt.Mul(&PBar, kf.Htilde.T())
-		HPHt.Mul(kf.Htilde, &PHt)
-		if ierr := HPHt.Inverse(&HPHt); ierr != nil {
-			return nil, fmt.Errorf("could not invert `H*P_kp1_minus*H' + R` at k=%d: %s", kf.step, ierr)
-		}
-		K.Mul(&PHt, &HPHt)
+	// Compute observation deviation y
+	var y mat64.Vector
+	y.SubVec(realObservation, computedObservation)
+	// Whiten the H and y
+	var Htilde mat64.Dense
+	Htilde.Mul(kf.sqrtInvNoise, kf.Htilde)
+	y.MulVec(kf.sqrtInvNoise, &y)
 
-		// Compute observation deviation y
-		var y mat64.Vector
-		y.SubVec(realObservation, computedObservation)
-
-		var innov, xHat mat64.Vector
-
-		// Prediction step.
-		var xBar mat64.Vector
-		xBar.MulVec(kf.Φ, kf.prevEst.State())
-		// Measurement update
-		var Hx mat64.Vector
-		Hx.MulVec(kf.Htilde, &xBar) // Predicted measurement
-		innov.SubVec(&y, &Hx)       // Innovation vector
-		// XXX: Does not support scalar measurements.
-		xHat.MulVec(&K, &innov)
-		xHat.AddVec(&xBar, &xHat)
-
-		var P, Ptmp1, IKH, KR, KRKt mat64.Dense
-		IKH.Mul(&K, kf.Htilde)
-		n, _ := IKH.Dims()
-		IKH.Sub(Identity(n), &IKH)
-		Ptmp1.Mul(&IKH, &PBar)
-		P.Mul(&Ptmp1, IKH.T())
-		KRKt.Mul(&KR, K.T())
-		P.Add(&P, &KRKt)
-
-		PBarSym, err := AsSymDense(&PBar)
-		if err != nil {
-			return nil, err
-		}
-
-		PSym, err := AsSymDense(&P)
-		if err != nil {
-			return nil, err
-		}
-		Φ := *mat64.DenseCopyOf(kf.Φ)
-		var Γ *mat64.Dense
-		est = NewSRIFEstimate(Φ, xHat, &y, innov, realObservation, R0, predR0)
-		kf.prevEst = *est
-		kf.step++
-		kf.locked = true
-	*/
+	Rk, bk, _, err := HouseholderSRIF(&RBar, &Htilde, &bBar, &y)
+	if err != nil {
+		return nil, err
+	}
+	kf.prevEst = NewSRIFEstimate(kf.Φ, bk, realObservation, &y, Rk, &RBar)
+	kf.step++
+	kf.locked = true
 	return
 }
 
@@ -158,7 +131,7 @@ func (kf *SRIF) fullUpdate(purePrediction bool, realObservation, computedObserva
 type SRIFEstimate struct {
 	Φ                            *mat64.Dense // Used for smoothing
 	sqinfoState, meas            *mat64.Vector
-	innov, Δobs, cachedState     *mat64.Vector
+	Δobs, cachedState            *mat64.Vector
 	matR0, predMatR0             *mat64.Dense
 	cachedCovar, predCachedCovar mat64.Symmetric
 }
@@ -194,11 +167,6 @@ func (e SRIFEstimate) State() *mat64.Vector {
 // Measurement implements the Estimate interface.
 func (e SRIFEstimate) Measurement() *mat64.Vector {
 	return e.meas
-}
-
-// Innovation implements the Estimate interface.
-func (e SRIFEstimate) Innovation() *mat64.Vector {
-	return e.innov
 }
 
 // ObservationDev returns the observation deviation.
@@ -244,27 +212,26 @@ func (e SRIFEstimate) String() string {
 	state := mat64.Formatted(e.State(), mat64.Prefix("  "))
 	meas := mat64.Formatted(e.Measurement(), mat64.Prefix("  "))
 	covar := mat64.Formatted(e.Covariance(), mat64.Prefix("  "))
-	innov := mat64.Formatted(e.Innovation(), mat64.Prefix("  "))
 	predp := mat64.Formatted(e.PredCovariance(), mat64.Prefix("   "))
-	return fmt.Sprintf("{\ns=%v\ny=%v\nP=%v\nP-=%v\ni=%v\n}", state, meas, covar, predp, innov)
+	return fmt.Sprintf("{\ns=%v\ny=%v\nP=%v\nP-=%v\n}", state, meas, covar, predp)
 }
 
 // NewSRIFEstimate initializes a new SRIFEstimate.
 // NOTE: R0 and predR0 are mat64.Dense for simplicity of implementation, but they should be symmetric.
-func NewSRIFEstimate(Φ *mat64.Dense, sqinfoState, meas, innov, Δobs *mat64.Vector, R0, predR0 *mat64.Dense) SRIFEstimate {
-	return SRIFEstimate{Φ, sqinfoState, meas, innov, Δobs, nil, R0, predR0, nil, nil}
+func NewSRIFEstimate(Φ *mat64.Dense, sqinfoState, meas, Δobs *mat64.Vector, R0, predR0 *mat64.Dense) SRIFEstimate {
+	return SRIFEstimate{Φ, sqinfoState, meas, Δobs, nil, R0, predR0, nil, nil}
 }
 
 // HouseholderSRIF prepare the matrix and performs the Householder transformation.
-func HouseholderSRIF(R, H *mat64.Dense, b, y *mat64.Vector) (*mat64.Dense, error) {
+func HouseholderSRIF(R, H *mat64.Dense, b, y *mat64.Vector) (*mat64.Dense, *mat64.Vector, *mat64.Vector, error) {
 	if err := checkMatDims(R, H, "R", "H", cols2cols); err != nil {
-		return nil, err
+		return nil, nil, nil, err
 	}
 	if err := checkMatDims(R, b, "R", "b", rows2rows); err != nil {
-		return nil, err
+		return nil, nil, nil, err
 	}
 	if err := checkMatDims(H, y, "H", "y", rows2rows); err != nil {
-		return nil, err
+		return nil, nil, nil, err
 	}
 	n, _ := b.Dims()
 	m, _ := y.Dims()
@@ -283,5 +250,18 @@ func HouseholderSRIF(R, H *mat64.Dense, b, y *mat64.Vector) (*mat64.Dense, error
 
 	HouseholderTransf(A, n, m)
 
-	return A, nil
+	// Extract the data.
+	Rk := A.Slice(0, n, 0, n)
+	bkMat := A.Slice(n, n+1, 0, n)
+	bk := mat64.NewVector(n, nil)
+	for i := 0; i < n; i++ {
+		bk.SetVec(i, bkMat.At(i, 0))
+	}
+	ekMat := A.Slice(n, n+1, n, m)
+	ek := mat64.NewVector(m-n, nil)
+	for i := 0; i < n; i++ {
+		ek.SetVec(i, ekMat.At(i, 0))
+	}
+
+	return Rk.(*mat64.Dense), bk, ek, nil
 }
--- srif_test.go
+++ srif_test.go
@@ -1,15 +1,20 @@
 package gokalman
 
 import (
+	"math"
 	"testing"
 
+	"github.com/gonum/matrix"
 	"github.com/gonum/matrix/mat64"
 )
 
 func TestSRIFR0(t *testing.T) {
 	x0 := mat64.NewVector(3, []float64{0, 0.35, 0})
 	P0 := ScaledIdentity(3, 10)
-	_, est0, err := NewSquareRootInformation(x0, P0, 3, true)
+	Q := mat64.NewSymDense(6, nil)
+	R := mat64.NewSymDense(2, []float64{math.Pow(5e-3, 2), 0, 0, math.Pow(5e-6, 2)})
+	noise := NewNoiseless(Q, R)
+	_, est0, err := NewSRIF(x0, P0, 3, true, noise)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -17,5 +22,8 @@ func TestSRIFR0(t *testing.T) {
 	if !mat64.Equal(est0.Covariance(), P0) {
 		t.Fatalf("difference in P0 and computed covariance:\n%+v\n%+v", mat64.Formatted(P0), mat64.Formatted(est0.Covariance()))
 	}
+
+	mat64.NewTriDense(3, matrix.Upper, nil)
+	t.Logf("\n%+v\n", mat64.Formatted(est0.matR0))
 
 }
